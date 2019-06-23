7baab9d4af620052772fd803ecbef8cd8ad943bb
Oscar M <oscarm@Oscars-MacBook-Pro.local>
2019-06-22 20:58:08 -0500

    first commit

---
+++ core/modules/datetime/tests/src/Unit/Plugin/migrate/field/DateFieldTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\Tests\datetime\Unit\Plugin\migrate\field;
+
+use Drupal\datetime\Plugin\migrate\field\DateField;
+use Drupal\migrate\MigrateException;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * @group migrate
+ */
+class DateFieldTest extends UnitTestCase {
+
+  /**
+   * Tests an Exception is thrown when the field type is not a known date type.
+   */
+  public function testUnknownDateType($method = 'defineValueProcessPipeline') {
+    $migration = $this->prophesize('Drupal\migrate\Plugin\MigrationInterface')->reveal();
+    $plugin = new DateField([], '', []);
+
+    $this->expectException(MigrateException::class);
+    $this->expectExceptionMessage("Field field_date of type 'timestamp' is an unknown date field type.");
+    $plugin->$method($migration, 'field_date', ['type' => 'timestamp']);
+  }
+
+}
